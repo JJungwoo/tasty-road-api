614cc62a4e5f9ee88092dddd5132a363b4549af3
JJungwoo <jungwoo5759@gmail.com>
2022-05-17 22:51:37 +0900

    feature: add elasticsearch client dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ repositories {
 }
 
 dependencies {
+    // elasticsearch
+    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+
     // swagger
     implementation("io.springfox:springfox-swagger2:2.9.2")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
